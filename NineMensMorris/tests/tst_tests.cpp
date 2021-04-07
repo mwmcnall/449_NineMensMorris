bab8fcd44f13b5cee42bae7cfe0c0aa3a1f97235
mwmcnall <mwmcnall@gmail.com>
2021-04-07 11:54:51 -0500

    more mill tests

--- NineMensMorris/tests/tst_tests.cpp
+++ NineMensMorris/tests/tst_tests.cpp
@@ -18,9 +18,14 @@ private slots:
     void cleanupTestCase();
     void testPlacePiece();
     void testDoNOTPlacePiece();
-    void testChooseFirstTurn();
-    void testBoardGUI();
-    void testMill();
+    void testChooseFirstTurn1();
+    void testChooseFirstTurn2();
+    void testMillHorizontal1();
+    void testMillHorizontal2();
+    void testMillHorizontal3();
+    void testMillVertical1();
+    void testMillVertical2();
+    void testMillVertical3();
 };
 
 tests::tests()
@@ -55,10 +60,13 @@ void tests::testPlacePiece()
     g->setTurn(1);
     g->SimulateButtonPress(0, 0);
 
-    Hole* button = &g->b->buttons[0][0];
+    Hole* button = g->getHole(0, 0);
+    g->setTurn(1);
+    player* p = g->getActivePlayer();
 
     QCOMPARE(button->filled, true);
     QCOMPARE(button->playerOwned, 1);
+    QCOMPARE(p->numPieces, 8);
 }
 
 void tests::testDoNOTPlacePiece() {
@@ -74,39 +82,103 @@ void tests::testDoNOTPlacePiece() {
     g->SimulateButtonPress(0, 0);
 
     Hole* button = g->getHole(0, 0);
+    g->setTurn(2);
+    player* p = g->getActivePlayer();
 
     // Make sure the button is filled
     QCOMPARE(button->filled, true);
     // Make sure ownership didn't change after being clicked a second time
     QCOMPARE(button->playerOwned, 1);
+    QCOMPARE(p->numPieces, 9);
 }
 
-void tests::testChooseFirstTurn()
+void tests::testChooseFirstTurn1()
 {
     int testTurn = 1;
 
     game *g = new game;
 
     g->setTurn(testTurn);
+    player* p = g->getActivePlayer();
     QCOMPARE(testTurn, g->getTurn());
+    QCOMPARE(p->turn, 1);
+}
+void tests::testChooseFirstTurn2()
+{
+    int testTurn = 2;
+
+    game *g = new game;
+
+    g->setTurn(testTurn);
+    player* p = g->getActivePlayer();
+    QCOMPARE(testTurn, g->getTurn());
+    QCOMPARE(p->turn, 2);
 }
 
-void tests::testBoardGUI()
-{
-    Board *board = new Board();
 
-    // Set to show buttons for debugging purposes
-    board->CreateBoardGUI(true)->show();
+// AC - 8.1
+// Successful horizontal mill detected
+void tests::testMillHorizontal1() {
+    game *g = new game;
+    g->b->CreateBoardGUI();
+    g->ConnectButtons();
+    // Make sure all moves are by same player
+    g->setTurn(1);
+    g->SimulateButtonPress(0, 0);
+    g->setTurn(1);
+    g->SimulateButtonPress(3, 0);
+    g->setTurn(1);
+    g->SimulateButtonPress(6, 0);
+    g->setTurn(1);
+    player* p = g->getActivePlayer();
 
-    // GUI disappears too quickly w/out a sleep command
-    QThread::sleep(4);
-
-    board->w->close();
+    // Does player think they got a mill?
+    QCOMPARE(p->inMill, true);
 }
 
-//Test Mill
-void tests::testMill()
-{
+// AC - 8.3
+// Horizontal mill NOT detected
+// Because of different player owning the pieces
+void tests::testMillHorizontal2() {
+    game *g = new game;
+    g->b->CreateBoardGUI();
+    g->ConnectButtons();
+    // Make sure all moves are by same player
+    g->setTurn(1);
+    g->SimulateButtonPress(0, 0);
+    g->setTurn(2);
+    g->SimulateButtonPress(3, 0);
+    g->setTurn(1);
+    g->SimulateButtonPress(6, 0);
+    g->setTurn(1);
+    player* p = g->getActivePlayer();
+
+    // Does player think they got a mill?
+    QCOMPARE(p->inMill, false);
+}
+
+// AC - 8.4
+// Horizontal mill NOT detected
+// Because three pieces not placed
+void tests::testMillHorizontal3() {
+    game *g = new game;
+    g->b->CreateBoardGUI();
+    g->ConnectButtons();
+    // Make sure all moves are by same player
+    g->setTurn(1);
+    g->SimulateButtonPress(0, 0);
+    g->setTurn(1);
+    g->SimulateButtonPress(3, 0);
+    g->setTurn(1);
+    player* p = g->getActivePlayer();
+
+    // Does player think they got a mill?
+    QCOMPARE(p->inMill, false);
+}
+
+// AC - 8.2
+// Successful vertical mill detected
+void tests::testMillVertical1() {
     game *g = new game;
     g->b->CreateBoardGUI();
     g->ConnectButtons();
@@ -122,8 +194,48 @@ void tests::testMill()
 
     // Does player think they got a mill?
     QCOMPARE(p->inMill, true);
-
 }
+
+// AC - 8.5
+// Vertical mill NOT detected
+// Because three pieces not placed
+void tests::testMillVertical2() {
+    game *g = new game;
+    g->b->CreateBoardGUI();
+    g->ConnectButtons();
+    // Make sure all moves are by same player
+    g->setTurn(1);
+    g->SimulateButtonPress(0, 0);
+    g->setTurn(2);
+    g->SimulateButtonPress(0, 3);
+    g->setTurn(1);
+    g->SimulateButtonPress(0, 6);
+    g->setTurn(1);
+    player* p = g->getActivePlayer();
+
+    // Does player think they got a mill?
+    QCOMPARE(p->inMill, false);
+}
+
+// AC - 8.6
+// Vertical mill NOT detected
+// Because three pieces not placed
+void tests::testMillVertical3() {
+    game *g = new game;
+    g->b->CreateBoardGUI();
+    g->ConnectButtons();
+    // Make sure all moves are by same player
+    g->setTurn(1);
+    g->SimulateButtonPress(0, 0);
+    g->setTurn(1);
+    g->SimulateButtonPress(0, 3);
+    g->setTurn(1);
+    player* p = g->getActivePlayer();
+
+    // Does player think they got a mill?
+    QCOMPARE(p->inMill, false);
+}
+
 
 //Test Remove
 
